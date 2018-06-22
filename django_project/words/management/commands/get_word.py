4d025eb44a2c1283537b3e425e35bfd04b09cb83
BgmLover <787782453@qq.com>
2018-06-22 22:54:59 +0800

    shit

---
+++ django_project/words/management/commands/get_word.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from words.models import get_one_unknown_word, get_one_known_word
+
+class Command(BaseCommand):
+    help = 'Please add the book_name'
+
+    def add_arguments(self, parser):
+        parser.add_argument('type', type=str)
+
+    def handle(self, *args, **options):
+        get_type = options['type']
+        if get_type == 'unknown':
+            get_one_unknown_word('zq', 'TOEFL_Basis')
+        else:
+            get_one_known_word('zq', 'TOEFL_Basis')
+
